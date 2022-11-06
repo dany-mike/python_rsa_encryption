6c34d75b14d7aa347d0e8b30db4ee64621c4b1c5
Dany <danymike.joseph@gmail.com>
2022-11-06 18:12:56 +0100

    get p n and phi done

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ q = 3
 n = p * q
 phi = (p-1)*(q-1)
 
+for i in range(1, phi):
+    print(i)
+print('end of loop')
+
 print("p: " + str(p) + " q: " + str(q))
 print("n: " + str(n))
 print("phi: " + str(phi))
