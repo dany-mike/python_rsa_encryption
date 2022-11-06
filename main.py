0ca3a23066e6b77155c764917eca06372065e24f
Dany <danymike.joseph@gmail.com>
2022-11-06 18:24:54 +0100

    generate e

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import random
+
+def generate_exclude_e(exclude_e, phi):
+    for i in range(2, phi):
+        if (phi % i == 0):
+            exclude_e.append(i)
+
+def give_e_value(exclude_e, phi):
+    while True:
+        e = random.randrange(2, phi)
+        if e not in exclude_e:
+            break
+    return e
+    
 p = 11
 q = 3
 n = p * q
 phi = (p-1)*(q-1)
+exclude_e = []
 
-for i in range(1, phi):
-    print(i)
-print('end of loop')
+generate_exclude_e(exclude_e, phi)
+
+print(exclude_e)
+
+e = give_e_value(exclude_e, phi)
 
 print("p: " + str(p) + " q: " + str(q))
 print("n: " + str(n))
 print("phi: " + str(phi))
+print("e: " + str(e))
